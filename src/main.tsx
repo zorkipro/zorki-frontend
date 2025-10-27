892f04f5046e14c749a789a9a849dac8eeb2df3b
Pumpkin-Pie93 <polinagreenheart@gmail.com>
2025-10-27 16:04:06 +0300

    added QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import "./index.css";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
     <App />
+      </QueryClientProvider>
   </HelmetProvider>,
 );
