84b4e08bcbcebce5a55aa87a141de7834c3073d0
leonid_shuliak <zeleny777@gmail.com>
2025-12-08 20:20:59 +0300

    add general style to mobile version -3

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -37,7 +37,7 @@ const DialogContent = React.forwardRef<
       ref={ref}
       className={cn(
         "fixed z-50 grid gap-4 border bg-background shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
-        "left-1/2 top-[25%] -translate-x-1/2 w-[calc(100%-2rem)] max-h-[70vh] overflow-y-auto p-4 rounded-lg",
+        "left-1/2 top-[10%] -translate-x-1/2 w-[calc(100%-2rem)] max-h-[80vh] overflow-y-auto p-4 rounded-lg",
         "sm:top-[50%] sm:-translate-y-1/2 sm:w-full sm:max-w-lg sm:max-h-[90vh] sm:p-6 sm:rounded-lg",
         "data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom",
         "sm:data-[state=closed]:slide-out-to-left-1/2 sm:data-[state=closed]:slide-out-to-top-[48%] sm:data-[state=open]:slide-in-from-left-1/2 sm:data-[state=open]:slide-in-from-top-[48%]",
